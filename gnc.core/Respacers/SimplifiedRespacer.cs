7138b1a6cbbdc024f4bb492acb736e0a6d169b61
dninemfive <11084010+dninemfive@users.noreply.github.com>
2026-03-21 14:40:49 -0600

    feat: simplified respacer view

--- gnc.core/Respacers/SimplifiedRespacer.cs
+++ gnc.core/Respacers/SimplifiedRespacer.cs
@@ -7,16 +7,17 @@ namespace d9.gnc.core.Respacers;
 /// Chunks text into standard units for mapping with the cipher. In the original Naibbe cipher,
 /// this will consist of only unigrams and bigrams.
 /// </summary>
-public class SimplifiedRespacer(Random? random = null) 
+public class SimplifiedRespacer(double unigramProportion = 0.5, Random? random = null) 
     : ITextRespacer
 {
     public readonly Random Random = random ?? new();
+    public double UnigramProportion { get; set; } = unigramProportion;
     public async IAsyncEnumerable<RespacedWord> RespaceAsync(string text)
     {
         int index = 0;
         while (index < text.Length)
         {
-            int length = Random.Next(1, 3);
+            int length = Random.NextDouble() < UnigramProportion ? 1 : 0; 
             yield return RespacedWord.FromUnigramOrBigram(text.SubstringSafe(index, length));
             index += length;
         }
