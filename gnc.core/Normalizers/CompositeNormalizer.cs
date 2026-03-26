1721be4e877e8b3c5eb411d55e5ab8a829214653
dninemfive <11084010+dninemfive@users.noreply.github.com>
2026-03-26 11:54:19 -0600

    feat: a lot of stuff (#5)

--- gnc.core/Normalizers/CompositeNormalizer.cs
+++ gnc.core/Normalizers/CompositeNormalizer.cs
@@ -3,9 +3,10 @@
 public class CompositeNormalizer(params IEnumerable<ITextNormalizer> normalizers)
     : ITextNormalizer
 {
+    public IEnumerable<ITextNormalizer> Components => normalizers;
     public async Task<string> NormalizeAsync(string text)
     {
-        foreach(ITextNormalizer normalizer in normalizers)
+        foreach(ITextNormalizer normalizer in Components)
             text = await normalizer.NormalizeAsync(text);
         return text;
     }
